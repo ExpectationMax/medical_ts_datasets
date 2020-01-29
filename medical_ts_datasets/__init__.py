f6c00aa624eddbf5b5a4bfedf3cee19b8c0ebf64
Max Horn <max.horn@bsse.ethz.ch>
2020-01-29 14:30:55 +0100

    Add `builders` attribute to main module

--- medical_ts_datasets/__init__.py
+++ medical_ts_datasets/__init__.py
@@ -5,4 +5,11 @@ import medical_ts_datasets.physionet_2019
 import medical_ts_datasets.mimic_3_phenotyping
 import medical_ts_datasets.mimic_3_mortality
 
+builders = [
+    'physionet2012',
+    'physionet2019',
+    'mimic3_mortality',
+    'mimic3_phenotyping'
+]
+
 __version__ = '0.1.0'
